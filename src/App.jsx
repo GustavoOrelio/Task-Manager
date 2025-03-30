ff560a7143680ce5f9a8fb5b4a3889282241d50b
Gustavo Orelio <oreliogustavo@gmail.com>
2025-03-29 23:13:53 -0300

    chore: add tailwind prettier plugin

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div>
-      <h1 className="text-red-500">Task Manager</h1>
+      <h1 className="mt-10 text-4xl font-bold text-red-500">Task Manager</h1>
     </div>
   );
 }
