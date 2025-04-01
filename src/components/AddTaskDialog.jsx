5be74ee1aead5fe65674765b3c23981f68246354
Gustavo Orelio <oreliogustavo@gmail.com>
2025-03-31 22:30:10 -0300

    perf: ensure inputs are uncontrolled on AddTaskDialog

--- src/components/AddTaskDialog.jsx
+++ src/components/AddTaskDialog.jsx
@@ -1,6 +1,6 @@
 import "./AddTaskDialog.css";
 
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
 import { createPortal } from "react-dom";
 import { CSSTransition } from "react-transition-group";
 import { v4 } from "uuid";
@@ -10,42 +10,38 @@ import Input from "./Input";
 import TimeSelect from "./TimeSelect";
 
 const AddTaskDialog = ({ isOpen, handleClose, handleSubmit }) => {
-  const [title, setTitle] = useState();
-  const [time, setTime] = useState("morning");
-  const [description, setDescription] = useState();
   const [errors, setErrors] = useState([]);
 
   const nodeRef = useRef();
-
-  useEffect(() => {
-    if (!isOpen) {
-      setTitle("");
-      setTime("morning");
-      setDescription("");
-    }
-  }, [isOpen]);
+  const titleRef = useRef();
+  const descriptionRef = useRef();
+  const timeRef = useRef();
 
   const handleSaveClick = () => {
     const newErrors = [];
 
+    const title = titleRef.current.value;
+    const description = descriptionRef.current.value;
+    const time = timeRef.current.value;
+
     if (!title.trim()) {
       newErrors.push({
         inputName: "title",
-        message: "O título é obrigatório",
+        message: "O título é obrigatório.",
       });
     }
 
     if (!time.trim()) {
       newErrors.push({
         inputName: "time",
-        message: "O horário é obrigatório",
+        message: "O horário é obrigatório.",
       });
     }
 
     if (!description.trim()) {
       newErrors.push({
         inputName: "description",
-        message: "A descrição é obrigatória",
+        message: "A descrição é obrigatória.",
       });
     }
 
@@ -73,18 +69,19 @@ const AddTaskDialog = ({ isOpen, handleClose, handleSubmit }) => {
 
   return (
     <CSSTransition
+      nodeRef={nodeRef}
       in={isOpen}
       timeout={500}
       classNames="add-task-dialog"
       unmountOnExit
-      nodeRef={nodeRef}
     >
       <div>
         {createPortal(
           <div
             ref={nodeRef}
-            className="fixed bottom-0 left-0 right-0 top-0 flex h-screen w-screen items-center justify-center bg-black/50 backdrop-blur"
+            className="fixed bottom-0 left-0 top-0 flex h-screen w-screen items-center justify-center backdrop-blur"
           >
+            {/* DIALOG */}
             <div className="rounded-xl bg-white p-5 text-center shadow">
               <h2 className="text-xl font-semibold text-[#35383E]">
                 Nova Tarefa
@@ -92,28 +89,26 @@ const AddTaskDialog = ({ isOpen, handleClose, handleSubmit }) => {
               <p className="mb-4 mt-1 text-sm text-[#9A9C9F]">
                 Insira as informações abaixo
               </p>
-              <div className="flex w-[336px] flex-col gap-4">
+
+              <div className="flex w-[336px] flex-col space-y-4">
                 <Input
                   id="title"
                   label="Título"
                   placeholder="Insira o título da tarefa"
-                  value={title}
-                  onChange={(e) => setTitle(e.target.value)}
                   errorMessage={titleError?.message}
+                  ref={titleRef}
                 />
-                <TimeSelect
-                  value={time}
-                  onChange={(e) => setTime(e.target.value)}
-                  errorMessage={timeError?.message}
-                />
+
+                <TimeSelect errorMessage={timeError?.message} ref={timeRef} />
+
                 <Input
                   id="description"
                   label="Descrição"
                   placeholder="Descreva a tarefa"
-                  value={description}
-                  onChange={(e) => setDescription(e.target.value)}
                   errorMessage={descriptionError?.message}
+                  ref={descriptionRef}
                 />
+
                 <div className="flex gap-3">
                   <Button
                     size="large"
--- src/components/TimeSelect.jsx
+++ src/components/TimeSelect.jsx
@@ -1,24 +1,30 @@
+import { forwardRef } from "react";
+
 import InputLabel from "./InputLabel";
 
-const TimeSelect = (props) => {
+const TimeSelect = forwardRef((props, ref) => {
   return (
     <div className="flex flex-col gap-1 text-left">
       <InputLabel htmlFor="time">Horário</InputLabel>
+
       <select
-        name="time"
         id="time"
-        className="rounded-lg border border-[#ECECEC] px-4 py-3 outline-[#00ADB5] placeholder:text-sm placeholder:text-[#9A9C9F]"
+        className="rounded-lg border border-solid border-[#ECECEC] px-4 py-3 outline-[#00ADB5] placeholder:text-sm placeholder:text-[#9A9C9F]"
         {...props}
+        ref={ref}
       >
         <option value="morning">Manhã</option>
         <option value="afternoon">Tarde</option>
         <option value="evening">Noite</option>
       </select>
+
       {props.errorMessage && (
         <p className="text-left text-xs text-red-500">{props.errorMessage}</p>
       )}
     </div>
   );
-};
+});
+
+TimeSelect.displayName = "TimeSelect";
 
 export default TimeSelect;
